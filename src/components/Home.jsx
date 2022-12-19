18f8ab7cb0d6bef752ba87bb5fcf5027b65cab93
francis omondi <francisomondi17@gmail.com>
2022-12-20 00:02:47 +0300

    polish the potfolio button

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -8,15 +8,18 @@ const Home = () => {
             <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row'>
                 <div className='flex flex-col justify-center h-full'>
                     <h2 className='text-4xl sm:text-7xl font-bold text-white'>I'm a Full Stack Developer</h2>
-                    <p className='text-gray-500 py-4 max-w-md'>I am a solution-driven professional excelling in highly collaborative work
+                    <p className='text-gray-500 py-4 max-w-md'>I am a solution-driven
+                        professional excelling in highly collaborative work
                         environment, finding solutions to challenges and focused on customer
                         satisfaction. Proven experience developing consumer-focused back and
                         front-end solutions in JavaScript node Js mongoDB, express and react
                         (MERN Stack)</p>
                     <div>
-                        <button className='text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-900'>Potfolio
-                            <span>
-                                <RxDoubleArrowRight />
+                        <button className='group text-white w-fit px-6 py-3 my-2 flex 
+                        items-center rounded-md bg-gradient-to-r from-cyan-500
+                         to-blue-900 cursor-pointer'>Potfolio
+                            <span className='group-hover:rotate-90 duration-300 '>
+                                <RxDoubleArrowRight size={30} className="ml-2" />
                             </span>
                         </button>
                     </div>
