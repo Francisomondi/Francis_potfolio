229fe03aafc5eafe3039a4a68abbf1112f42b147
francis omondi <francisomondi17@gmail.com>
2023-01-05 06:37:35 +0300

    Experience jsx display

---
+++ src/components/Experience.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import css from "../assets/css.png"
+import github from "../assets/github.png"
+import html from "../assets/html.png"
+import javascript from "../assets/javascript.png"
+import nextjs from "../assets/node.png"
+import node from "../assets/nextjs.png"
+import reactImage from "../assets/react.png"
+import tailwind from "../assets/tailwind.png"
+
+
+const Experience = () => {
+    return (
+        <div>
+            <div>
+                <div>
+                    <p>Experience</p>
+                    <p>These are the technologies i have worked with</p>
+                </div>
+            </div>
+
+            <div>
+                <div>
+                    <img src={html} alt='#' />
+                    <p>HTML</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Experience
