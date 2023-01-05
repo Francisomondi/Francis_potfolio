b8c39cda42e521dce2f5654776f183817eeb053f
francis omondi <francisomondi17@gmail.com>
2023-01-05 07:56:32 +0300

    looping through experiences and displaying results

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,42 +10,58 @@ import tailwind from "../assets/tailwind.png"
 
 
 const Experience = () => {
-    {
-        const experiences = [
-            {
-                id: 1,
-                img: css
-            },
-            {
-                id: 2,
-                img: github
-            },
-            {
-                id: 3,
-                img: html
-            },
-            {
-                id: 4,
-                img: javascript
-            },
-            {
-                id: 5,
-                img: nextjs
-            },
-            {
-                id: 6,
-                img: node
-            },
-            {
-                id: 7,
-                img: reactImage
-            },
-            {
-                id: 8,
-                img: tailwind
-            },
-        ]
-    }
+
+    const experiences = [
+        {
+            id: 1,
+            src: css,
+            title: "css",
+            style: "shadow-blue-500"
+        },
+        {
+            id: 2,
+            src: github,
+            title: "Github",
+            style: "shadow-gray-400"
+        },
+        {
+            id: 3,
+            src: html,
+            title: "HTML",
+            style: "shadow-orange-500"
+        },
+        {
+            id: 4,
+            src: javascript,
+            title: "Javascript",
+            style: "shadow-yellow-500"
+        },
+        {
+            id: 5,
+            src: nextjs,
+            title: "Nextjs",
+            style: "shadow-white"
+        },
+        {
+            id: 6,
+            src: node,
+            title: "Node",
+            style: "shadow-green-500"
+        },
+        {
+            id: 7,
+            src: reactImage,
+            title: "react",
+            style: "shadow-blue-600"
+        },
+        {
+            id: 8,
+            src: tailwind,
+            title: "Tailwind",
+            style: "shadow-sky-400"
+        },
+    ]
+
     return (
         <div name="experience" className='bg-gradient-to-b from-gray-800 to-black w-full h-screen'>
 
@@ -56,10 +72,16 @@ const Experience = () => {
                 </div>
 
                 <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center py-8 px-12 sm:px-0'>
-                    <div className='shadow-md hover:scale-105 duration-500 py-2 rounded-lg'>
-                        <img src={html} alt='#' className='w-20 mx-auto' />
-                        <p className='mt-4'>HTML</p>
-                    </div>
+
+                    {
+                        experiences.map(({ id, src, title, style }) => (
+                            <div key={id} className={`shadow-md hover:scale-105 duration-500 py-2 rounded- ${style}`}>
+                                <img src={src} alt='#' className='w-20 mx-auto' />
+                                <p className='mt-4'>{title}</p>
+                            </div>
+                        ))
+                    }
+
                 </div>
             </div>
 
