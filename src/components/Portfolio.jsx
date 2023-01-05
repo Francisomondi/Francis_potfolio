067d3f40d7e9510d0af6ab4feaebde4e9e2b5c3f
francis omondi <francisomondi17@gmail.com>
2023-01-05 06:12:39 +0300

    looping through portfolio and displaying all

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,9 +5,36 @@ import navbar from "../assets/portfolio/navbar.jpg"
 import reactParallax from "../assets/portfolio/reactParallax.jpg"
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg"
 import reactWeather from "../assets/portfolio/reactWeather.jpg"
-import usestate from "../assets/portfolio/usestate.jpg"
+
 
 const Portfolio = () => {
+    const portfolios = [
+        {
+            id: 1,
+            src: arrayDestruct
+        },
+        {
+            id: 2,
+            src: installNode
+        },
+        {
+            id: 3,
+            src: navbar
+        },
+        {
+            id: 4,
+            src: reactParallax
+        },
+        {
+            id: 5,
+            src: reactSmooth
+        },
+        {
+            id: 6,
+            src: reactWeather
+        },
+    ]
+
     return (
         <div name="portfolio" className='bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
             <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-between w-full h-full'>
@@ -16,14 +43,24 @@ const Portfolio = () => {
                     <p className='py-6'>Check out some of my Work here </p>
                 </div>
 
+
+
                 <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0'>
-                    <div className='shadow-md shadow-gray-600 rounded-lg'>
-                        <img src={reactWeather} alt="reactWeather" className='rounded-md duration-200 hover:scale-105' />
-                        <div className='flex items-center justify-center'>
-                            <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>copy</button>
-                            <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>paste</button>
-                        </div>
-                    </div>
+                    {
+                        portfolios.map(({ id, src }) => {
+                            return (
+                                <div key={id} className='shadow-md shadow-gray-600 rounded-lg'>
+                                    <img src={src} alt="reactWeather" className='rounded-md duration-200 hover:scale-105' />
+                                    <div className='flex items-center justify-center'>
+                                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Demo</button>
+                                        <button className='w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105'>Code</button>
+                                    </div>
+                                </div>
+                            )
+
+                        })
+                    }
+
                 </div>
             </div>
         </div>
